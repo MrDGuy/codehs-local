b04703d52b5e5e0e98b6574638ae4d83fb48b243
MrDGuy <david.guy@apsk12.org>
2025-06-23 14:04:04 -0400

    Updates

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 //Write your code here:
 println("Hello world");
+let length = readInt("Enter the rectangle length:")
+let width = readInt("Enter the rectangle width");
+let rect = new Rectangle(length, width);
+rect.setPosition(getWidth()/2-width/2, getHeight()/2-length);
+add(rect);
+keyDownMethod(callbackFunction)
+function callbackFunction(e){
+    let jazz = new Audio("https://codehs.com/uploads/8179d3793582c02c44438d0160d6f0a5");
+    jazz.play();
+}
+ 
