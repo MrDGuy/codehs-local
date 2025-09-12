9daef879ff28e23f8239c3c621744ea73ec5bf3d
MrDGuy <david.guy@apsk12.org>
2025-09-12 15:52:29 -0400

    Updated with working Console

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-//Write your code here:
+// @ts-nocheck
 println("Hello world");
-let length = readInt("Enter the rectangle length:")
-let width = readInt("Enter the rectangle width");
-let rect = new Rectangle(length, width);
-rect.setPosition(getWidth()/2-width/2, getHeight()/2-length);
+
+let rectLength = readInt("Enter the rectangle length:");
+let rectWidth  = readInt("Enter the rectangle width:");
+
+let rect = new Rectangle(rectWidth, rectLength);
+rect.setPosition((getWidth() - rectWidth) / 2, (getHeight() - rectLength) / 2);
 add(rect);
 
-keyDownMethod(callbackFunction)
-function callbackFunction(e){
-    let jazz = new Audio("bird.mp3");
-    jazz.play()
+let c = new Circle(45);
+c.setPosition(0, getHeight() / 2);
+add(c);
+
+setTimer(moveCircle, 40);
+function moveCircle() {
+    if (c.getX() < getWidth() + 45) {
+        c.move(5, 0);
+    }
 }
 
-let c = new Circle(45);
+keyDownMethod(playAudio);
+function playAudio(e) {
+    if (e.keyCode === Keyboard.letter("K")) {
+        let jazz = new Audio("bird.mp3");
+        jazz.play();
+    } else if (e.keyCode === Keyboard.letter("J")) {
+        let mySound = new Sound("C4", "square");
+        mySound.playFor(5);
+    }
+}
